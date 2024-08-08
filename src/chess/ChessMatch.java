c7533bd4ce416c6049e8ea00f37f69304ae23518
Augusto Ludewig <augustoludewig16@gmail.com>
2024-08-08 09:27:46 -0300

    Colocando as peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,10 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Queen;
+import chess.pieces.Roock;
 
 // Classe com as regras do jogo
 
@@ -10,6 +14,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8,8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -19,6 +24,22 @@ public class ChessMatch {
 				mat[i][j] = (ChessPiece)board.piece(i, j);
 			}
 		} return mat;
+	}
+	
+	private void initialSetup() { 	//POSIÇÕES INICIAIS DAS PEÇAS NO TABULEIRO
+		// PEÇAS BRANCAS
+		board.placePiece(new Roock(board, Color.WHITE), new Position(0, 0)); 	//CAVALO
+		board.placePiece(new Roock(board, Color.WHITE), new Position(0, 7));	//CAVALO
+		board.placePiece(new King(board, Color.WHITE), new Position(0, 4));
+		board.placePiece(new Queen(board, Color.WHITE), new Position(0, 3));
+		
+		
+		// PEÇAS PRETAS
+		board.placePiece(new Roock(board, Color.BLACK), new Position(7, 0));
+		board.placePiece(new Roock(board, Color.BLACK), new Position(7, 7));
+		board.placePiece(new King(board, Color.BLACK), new Position(7, 4));
+		board.placePiece(new Queen(board, Color.BLACK), new Position(7, 3));
+		
 	}
 	
 	
